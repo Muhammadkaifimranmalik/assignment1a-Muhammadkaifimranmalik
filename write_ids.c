eac929a00fe4120faff2608e34c1ecd00390e2b5
Muhammad Kaif Imran Malik <mki5151@psu.edu>
2024-09-16 15:37:09 -0400

    Assignment 1A

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "Muhammadkaifimranmalik";
+    char STUDENT_ID[] = "987716632";
 
     return 0 ;
 }
